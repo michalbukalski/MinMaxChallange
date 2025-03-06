7869bad2d3d666b96d0782d7a4dfcf7f097f6229
michalbukalski <michalbukalski@gmail.com>
2025-03-06 01:47:08 +0100

    challange finished

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int max = 0;
+        int min = 0;
+        int loopCount=0;
+
+        while (true){
+            System.out.println("Enter a number, or any character to exit");
+            String nextEntry = scanner.nextLine();
+            try{
+                int validNum=Integer.parseInt(nextEntry);
+                if (loopCount == 0 || validNum < min) {
+                    min = validNum;
+                }
+                if (loopCount == 0 || validNum > max) {
+                    max = validNum;
+                }
+                loopCount++;
+            } catch (NumberFormatException e){
+                break;
+            }
+
+        }
+        if(loopCount>0) {
+            System.out.println("Min value is " + min);
+            System.out.println("Max value is " + max);
+        } else {
+            System.out.println("No valid data entered.");
+        }
+    }
 }
